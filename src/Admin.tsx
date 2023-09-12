9888e2fd55685eac551872f87a71aa2852e4141c
Cory House <housecor@gmail.com>
2023-09-12 13:56:46 -0500

    add

--- src/Admin.tsx
+++ src/Admin.tsx
@@ -1,5 +1,20 @@
 import { Heading } from "./shared/Heading";
 
 export function Admin() {
-  return <Heading tag="h1">Admin</Heading>;
+  return (
+    <form>
+      <Heading tag="h1">Admin</Heading>
+      <label className="block" htmlFor="name">
+        Name
+      </label>
+      <input className="mb-2" id="name" type="text" />
+
+      <label className="block" htmlFor="description">
+        Description
+      </label>
+      <input className="mb-2" id="description" type="text" />
+
+      <input className="block" type="button" value="Add Food" />
+    </form>
+  );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -8,13 +8,15 @@ import { Admin } from "./Admin";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <nav className="mb-2 bg-orange-200 p-2">
+      <nav className="mb-2 bg-orange-200 p-2 pl-4">
         <Link to="/">Menu</Link> | <Link to="/admin">Admin</Link>
       </nav>
-      <Routes>
-        <Route path="/" element={<Menu />} />
-        <Route path="/admin" element={<Admin />} />
-      </Routes>
+      <main>
+        <Routes>
+          <Route path="/" element={<Menu />} />
+          <Route path="/admin" element={<Admin />} />
+        </Routes>
+      </main>
     </BrowserRouter>
   </React.StrictMode>
 );
